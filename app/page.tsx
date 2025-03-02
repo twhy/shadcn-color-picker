4cfeeffde24d48461e83cde8fab00c8b992b344e
Tom Wang <tom.wanghaiyi@gmail.com>
2025-03-03 01:20:11 +0800

    add github link

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import {
   hexToHSLA,
   hexToRGBA,
 } from "@/components/ui/color-picker";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   const [color, setColor] = useState<HexColor>("#00ffff");
@@ -33,17 +35,34 @@ export default function Home() {
           </span>
         </div>
         <div className="font-mono font-medium flex items-center justify-center">
-          RGBA({<ColorValue value={rgba.r} />}, {<ColorValue value={rgba.g} />}, {<ColorValue value={rgba.b} />}, {<ColorValue value={rgba.a.toFixed(2)} />})
+          RGBA({<ColorValue value={rgba.r} />}, {<ColorValue value={rgba.g} />},
+          {" "}
+          {<ColorValue value={rgba.b} />},{" "}
+          {<ColorValue value={rgba.a.toFixed(2)} />})
         </div>
         <div className="font-mono font-medium flex items-center justify-center">
-          HSLA({<ColorValue value={hsla.h} />}, {<ColorValue value={hsla.s} />}, {<ColorValue value={hsla.l} />}, {<ColorValue value={hsla.a.toFixed(2)} />})
+          HSLA({<ColorValue value={hsla.h} />}, {<ColorValue value={hsla.s} />},
+          {" "}
+          {<ColorValue value={hsla.l} />},{" "}
+          {<ColorValue value={hsla.a.toFixed(2)} />})
+        </div>
+        <div className="my-1 text-gray-600 text-sm font-mono font-medium hover:underline flex items-center space-x-2">
+          <Image src="/github.png" alt="GitHub" width={20} height={20} />
+          <Link
+            target="_blank"
+            href="https://github.com/twhy/shadcn-color-picker"
+          >
+            twhy/shadcn-color-picker
+          </Link>
         </div>
       </div>
     </main>
   );
 }
 
-function ColorValue({ value, size = 3 }: { value: number | string; size?: number }) {
+function ColorValue(
+  { value, size = 3 }: { value: number | string; size?: number },
+) {
   return (
     <pre className="text-gray-600 mx-1 tracking-wide font-mono font-medium uppercase">
       {String(value).padStart(size, " ")}
